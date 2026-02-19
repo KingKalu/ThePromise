ca9d8202d1213f92d4764e18ff9c311975fb9660
Isaac Kalu <kaluisaac580@gmail.com>
2026-02-19 15:48:36 +0100

    ....

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const next = require("next");
 
-const dev = process.env.NODE_ENV === "production";
-console.log({ dev });
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
